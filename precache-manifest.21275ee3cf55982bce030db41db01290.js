881fd596d6edef70665d996ecc3025a733a642c5
Akshay <akshsharma@deqode.com>
2020-12-17 01:06:08 +0530

    Updates

---
+++ precache-manifest.21275ee3cf55982bce030db41db01290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb91176a3a839bb2ca26cbdf04a7b63",
+    "url": "/akshay-task/index.html"
+  },
+  {
+    "revision": "c1074359cf6278e3d411",
+    "url": "/akshay-task/static/css/main.a07a1cda.chunk.css"
+  },
+  {
+    "revision": "0376f5ab68a05ac0564d",
+    "url": "/akshay-task/static/js/2.7bfd4ed3.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/akshay-task/static/js/2.7bfd4ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1074359cf6278e3d411",
+    "url": "/akshay-task/static/js/main.ce72bd0a.chunk.js"
+  },
+  {
+    "revision": "a5fbd52216343d168458",
+    "url": "/akshay-task/static/js/runtime-main.9544c1c6.js"
+  }
+]);
